def918e10d3dcf4fc84038997e9cb381af79ff89
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-04-06 14:15:26 -0400

    templates and css created

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# dummy data
+
+class Car:
+    def __init__(self, make, model, description, year):
+        self.make = make
+        self.model = model
+        self.description = description
+        self.year = year
+
+
+cars = [
+    Car('Lamborghini', 'Aventador', 'Very fast car', 2020),
+    Car('Rolls Royce', 'Wraith', 'Super Comfortable car', 2017),
+    Car('Mercedes Benz', 'S Class', 'Very elegant car', 2018)
+]
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('whats up')
+    return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+def cars_index(request):
+    return render(request, 'cars/index.html', {'cars': cars})
