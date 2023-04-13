8b46160c66bfb731faec437658767076ce1a087b
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-04-12 20:21:50 -0400

    migrations applied, driver wors now

--- main_app/models.py
+++ main_app/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-
-class Car(models.Model):
-    make = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    year = models.IntegerField(default=2020)
-
-    def __str__(self):
-        return self.make
-
-    def get_absolute_url(self):
-        return reverse('cars_details', kwargs={'car_id': self.id})
-    
 
 class Driver(models.Model):
     name = models.CharField(max_length=100)
@@ -25,6 +11,31 @@ class Driver(models.Model):
     
     def get_absolute_url(self):
         return reverse('drivers_detail', kwargs={'pk': self.id})
+    
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField(default=2020)
+    drivers = models.ManyToManyField(Driver)
+
+    def __str__(self):
+        return self.make
+
+    def get_absolute_url(self):
+        return reverse('cars_details', kwargs={'car_id': self.id})
+    
+
+# class Driver(models.Model):
+#     name = models.CharField(max_length=100)
+#     years_of_experience = models.IntegerField(default=10)
+
+#     def __str__(self):
+#         return self.name
+    
+#     def get_absolute_url(self):
+#         return reverse('drivers_detail', kwargs={'pk': self.id})
     
 
 class Service(models.Model):
