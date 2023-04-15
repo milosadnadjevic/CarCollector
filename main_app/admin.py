b5e5d3231fc8838c7aec60c36b5708d1477ea202
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-04-15 10:37:25 -0400

    login/logout added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Car, Service
+from .models import Car, Service, Driver
 
 admin.site.register(Car)
 admin.site.register(Service)
+admin.site.register(Driver)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
 
 class Driver(models.Model):
     name = models.CharField(max_length=100)
@@ -19,6 +19,7 @@ class Car(models.Model):
     description = models.TextField(max_length=250)
     year = models.IntegerField(default=2020)
     drivers = models.ManyToManyField(Driver)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.make
